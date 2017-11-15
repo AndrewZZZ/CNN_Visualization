18fe005fbc4c5e4013f54457f37690aac8fd9d8d
Andrew <andrew.zh@live.com>
2017-11-15 01:55:33 -0500

    init

--- train.py
+++ train.py
@@ -2,18 +2,42 @@ import torch
 import torchvision
 import argparse
 import importlib
-
+import os
+import numpy as np
 
 def main(args):
     #network = importlib.import_module(args.model_def)
-    network = torchvision.models.SqueezeNet
+    network = torchvision.models.squeezenet1_0()
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(network.parameters(), lr=0.001, momentum=0.9)
-    
+    for name,m in network.named_modules():
+        m.register_buffer('name', name)
+
+        
+
+
+def hook_model(network, hook):
+    handler_list = []
+    for m in network.models():
+        handler = m.register_forward_hook(hook)
+        handler_list.append(handler)
+    return handler_list
+
+def remove_hook(handler_list):
+    for handler in handler_list:
+        handler.remove()
 
 def vis_hook(model, input, output):
-    
-    
+    log_root = os.path.expanduser(args.vis_output_dir)
+    path_token_list = model.name.split('.')
+    log_dir = log_root
+    for path_token in path_token_list:
+        log_dir = os.path.join(log_dir, path_token)
+    if not os.path.isdir(log_dir):
+        os.makedirs(log_dir)
+    log_path = os.path.join(log_dir, 'vis_data.npy')
+    print('Writing visualization data into ', log_path)
+    np.save(log_path, output.data.numpy())
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
